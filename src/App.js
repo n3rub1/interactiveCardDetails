9d6671084436cd9ebe191db5920aaa4d54809d7f
Nerubi <sm4391@stmartins.edu>
2022-09-12 19:40:17 +0200

    adjusted html and css, to do year month cvc

--- src/App.js
+++ src/App.js
@@ -16,18 +16,35 @@ function App() {
   };
 
   return (
-    <div>
-      <div className="topDiv">
-        <section className="container">
-          <img className="frontCardSection" src={cardFront} alt="card front image"></img>
-          <img className="backCardSection" src={cardBack} alt="card back image"></img>
-        </section>
+    <div className="topBody">
+      <div className="top">
+        <div className="cardTop">
+          <div className="cardFront">
+            <img
+              src={cardFront}
+              alt="card front image"
+            ></img>
+            <div className="nameOfUser">{nameOfUser}</div>
+            <div className="cardNumber">{cardNumber}</div>
+            <div className="month">{expDateMonth}</div>
+            <div className="divider">/</div>
+            <div className="year">{expDateYear}</div>
+          </div>
+          <div className="cardBack">
+            <img
+              src={cardBack}
+              alt="card back image"
+            ></img>
+            <div className="cvc">{cvc}</div>
+          </div>
+        </div>
 
-        <div className="topForm">
-          <form onSubmit={handleSubmit}>
-            <label className="field">
+        <div className="formTop--div">
+          <form className="formTop" onSubmit={handleSubmit}>
+            <label className="nameLabel">
               Cardholder Name
               <input
+                className="nameInput"
                 placeholder="Eg: Jane Appleseed"
                 type="text"
                 value={nameOfUser}
@@ -38,9 +55,10 @@ function App() {
             </label>
 
             <div>
-              <label className="field">
+              <label className="cardLabel">
                 Card Number
                 <input
+                  className="cardInput"
                   placeholder="Eg: 1234 5678 9100 1234"
                   type="text"
                   value={cardNumber}
@@ -52,43 +70,42 @@ function App() {
             </div>
 
             <div className="month-year-cvc-top">
-              <div className="month-year-top">
-                <label className="field">
-                  Exp. Date (MM/YY)
-                  <input
-                    placeholder="MM"
-                    type="text"
-                    value={expDateMonth}
-                    onChange={(e) => {
-                      setExpDateMonth(e.target.value);
-                    }}
-                  />
-                  <input
-                    placeholder="YY"
-                    type="text"
-                    value={expDateYear}
-                    onChange={(e) => {
-                      setExpDateYear(e.target.value);
-                    }}
-                  />
-                </label>
-              </div>
-              <div className="month-year-top">
-                <label className="field">
-                  CVC
-                  <input
-                    placeholder="123"
-                    type="text"
-                    value={cvc}
-                    onChange={(e) => {
-                      setCvc(e.target.value);
-                    }}
-                  />
-                </label>
-              </div>
+              <label className="monthYearLabel">
+                Exp. Date (MM/YY)
+                <input
+                  className="monthInput"
+                  placeholder="MM"
+                  type="text"
+                  value={expDateMonth}
+                  onChange={(e) => {
+                    setExpDateMonth(e.target.value);
+                  }}
+                />
+                <input
+                  className="yearInput"
+                  placeholder="YY"
+                  type="text"
+                  value={expDateYear}
+                  onChange={(e) => {
+                    setExpDateYear(e.target.value);
+                  }}
+                />
+              </label>
+              <label className="cvcField">
+                CVC
+                <input
+                  className="cvcInput"
+                  placeholder="123"
+                  type="text"
+                  value={cvc}
+                  onChange={(e) => {
+                    setCvc(e.target.value);
+                  }}
+                />
+              </label>
             </div>
 
-            <button>Confirm</button>
+            <input type="submit" value="Submit"></input>
           </form>
         </div>
       </div>
